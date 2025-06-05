cd3fbbbd2c4d3586fb020d9951958af49fe30e7b
Gauravvgithub <gauravraj7011@gmail.com>
2025-06-05 22:47:10 +0530

    post user functionality achieve

--- models/DB.js
+++ models/DB.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const connectDB = async ()=>{
     try{
-        await mongoose.connect("mongodb+srv://admin:admin123@cluster0.2efopbe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+        await mongoose.connect("mongodb+srv://admin:admin123@cluster0.2efopbe.mongodb.net/Employee?retryWrites=true&w=majority&appName=Cluster0")
         console.log("Database connected succesfully")
     } catch (error){
         console.log(`something went wrong ${error}`)
